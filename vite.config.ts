da68dbb310095f75fb92c0eddc168bbad2a9f17a
TomatoHB <TomatoHB.me>
2026-07-03 16:36:58 +0800

    feat: docs

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "BigScreenDemo/",
+  base: "/BigScreenDemo/",
   resolve: {
     alias: {
       "@": resolve("src"),
